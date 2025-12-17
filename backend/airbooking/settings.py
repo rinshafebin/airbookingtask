c9429d01a9bf17fdc67d7f973ecc3a5e36f02883
rinshafebin <rinshafebinkk12@gmail.com>
2025-12-17 16:31:41 +0530

    added vercel file

--- backend/airbooking/settings.py
+++ backend/airbooking/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
 
